d92e4dea9b709efdd13f05eec739da6eb77f6ae6
Michael Matzdorff <mikematzdorff@gmail.com>
2026-01-05 14:48:30 -0800

    Remove Readonly wrapper for Next.js compatibility

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,9 +25,9 @@ const chapters = [
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode
-}>) {
+}) {
   return (
     <html lang="en">
       <body>
